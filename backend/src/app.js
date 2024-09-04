c2e3a924111d88d12ffcc83aaf77735098d4b6da
atif275 <atifhanif275@gmail.com>
2024-09-04 09:13:02 +0300

    Add Vps

--- backend/src/app.js
+++ backend/src/app.js
@@ -87,9 +87,7 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
 
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to RU-novel API');
-});
+
 
 
 app.get('/api/hello', (req, res) => {
