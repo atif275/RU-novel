1321165ef96219c77564da91c0a6bdc87b60a1d2
atif275 <atifhanif275@gmail.com>
2024-09-12 01:25:34 +0400

    f

--- backend/Controller/Oauth.js
+++ backend/Controller/Oauth.js
@@ -11,104 +11,83 @@ const clientSecret =  process.env.clientSecret;
 const facebookID = "1252397179082903";
 const facebookSecret = "149a03dccd816bb96e97a5adb18ecdfc";
 
-passport.use(
-	new GoogleStrategy(
-		{
-			clientID: process.env.CLIENT_ID,
-			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: "/auth/google/callback",
-			scope: ["profile", "email"],
-		},
-		function (accessToken, refreshToken, profile, callback) {
-			callback(null, profile);
-		}
-	)
-);
 
+passport.use(new GoogleStrategy({
+  clientID: process.env.clientID,
+  clientSecret: process.env.clientSecret,
+  callbackURL: '/auth/google/callback',
+  scope: ['profile', 'email'],
+},
+async (accessToken, refreshToken, profile, done) => {
+  try {
+    let user = await Userdb.findOne({ googleId: profile.id });
+    if (!user) {
+      // New user signing up
+      user = new Userdb({
+        googleId: profile.id,
+        username: profile.displayName,
+        email: profile.emails[0].value,
+        profilePicture: profile.photos[0].value
+      });
+      await user.save();
+      return done(null, { user, isNewUser: true });
+    }
+    // Existing user logging in
+    return done(null, { user, isNewUser: false });
+  } catch (err) {
+    done(err, null);
+  }
+}));
+
+
+passport.use(new FacebookStrategy({
+    clientID: facebookID,
+    clientSecret: facebookSecret,
+    callbackURL: '/auth/facebook/callback',
+    profileFields: ['id', 'displayName', 'photos', 'email']
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      const email = profile.emails && profile.emails[0] ? profile.emails[0].value : null;
+  
+      if (!email) {
+        // If no email is provided, return an error
+        return done(null, false, { message: 'Email is required to sign up' });
+      }
+  
+      let user = await Userdb.findOne({ facebookId: profile.id });
+      if (!user) {
+        // New user signing up
+        user = new Userdb({
+          facebookId: profile.id,
+          username: profile.displayName,
+          email: email,
+          profilePicture: profile.photos && profile.photos[0] ? profile.photos[0].value : null // Check if photos exist
+        });
+        await user.save();
+        return done(null, { user, isNewUser: true });
+      }
+      // Existing user logging in
+      return done(null, { user, isNewUser: false });
+    } catch (err) {
+      done(err, null);
+    }
+  }));
+  
+  
+
+// Serialize and deserialize user
 passport.serializeUser((user, done) => {
-	done(null, user);
+  done(null, user.user._id); // Assuming the user object has a user property containing user data
 });
 
-passport.deserializeUser((user, done) => {
-	done(null, user);
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await Userdb.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.clientID,
-//   clientSecret: process.env.clientSecret,
-//   callbackURL: '/auth/google/callback',
-//   scope: ['profile', 'email'],
-// },
-// async (accessToken, refreshToken, profile, done) => {
-//   try {
-//     let user = await Userdb.findOne({ googleId: profile.id });
-//     if (!user) {
-//       // New user signing up
-//       user = new Userdb({
-//         googleId: profile.id,
-//         username: profile.displayName,
-//         email: profile.emails[0].value,
-//         profilePicture: profile.photos[0].value
-//       });
-//       await user.save();
-//       return done(null, { user, isNewUser: true });
-//     }
-//     // Existing user logging in
-//     return done(null, { user, isNewUser: false });
-//   } catch (err) {
-//     done(err, null);
-//   }
-// }));
-
-
-// passport.use(new FacebookStrategy({
-//     clientID: facebookID,
-//     clientSecret: facebookSecret,
-//     callbackURL: 'https://api.ru-novel.ru/auth/facebook/callback',
-//     profileFields: ['id', 'displayName', 'photos', 'email']
-//   },
-//   async (accessToken, refreshToken, profile, done) => {
-//     try {
-//       const email = profile.emails && profile.emails[0] ? profile.emails[0].value : null;
-  
-//       if (!email) {
-//         // If no email is provided, return an error
-//         return done(null, false, { message: 'Email is required to sign up' });
-//       }
-  
-//       let user = await Userdb.findOne({ facebookId: profile.id });
-//       if (!user) {
-//         // New user signing up
-//         user = new Userdb({
-//           facebookId: profile.id,
-//           username: profile.displayName,
-//           email: email,
-//           profilePicture: profile.photos && profile.photos[0] ? profile.photos[0].value : null // Check if photos exist
-//         });
-//         await user.save();
-//         return done(null, { user, isNewUser: true });
-//       }
-//       // Existing user logging in
-//       return done(null, { user, isNewUser: false });
-//     } catch (err) {
-//       done(err, null);
-//     }
-//   }));
-  
-  
-
-// // Serialize and deserialize user
-// passport.serializeUser((user, done) => {
-//   done(null, user.user._id); // Assuming the user object has a user property containing user data
-// });
-
-// passport.deserializeUser(async (id, done) => {
-//   try {
-//     const user = await Userdb.findById(id);
-//     done(null, user);
-//   } catch (err) {
-//     done(err, null);
-//   }
-// });
-
-// module.exports = passport;
+module.exports = passport;
