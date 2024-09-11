7ba8ac75066f34d2ec166917c02192c4d5e49e79
atif275 <atifhanif275@gmail.com>
2024-09-12 02:11:33 +0400

    fsd

--- backend/Controller/Oauth.js
+++ backend/Controller/Oauth.js
@@ -41,39 +41,39 @@ async (accessToken, refreshToken, profile, done) => {
 }));
 
 
-passport.use(new FacebookStrategy({
-    clientID: facebookID,
-    clientSecret: facebookSecret,
-    callbackURL: '/auth/facebook/callback',
-    profileFields: ['id', 'displayName', 'photos', 'email']
-  },
-  async (accessToken, refreshToken, profile, done) => {
-    try {
-      const email = profile.emails && profile.emails[0] ? profile.emails[0].value : null;
+// passport.use(new FacebookStrategy({
+//     clientID: facebookID,
+//     clientSecret: facebookSecret,
+//     callbackURL: '/auth/facebook/callback',
+//     profileFields: ['id', 'displayName', 'photos', 'email']
+//   },
+//   async (accessToken, refreshToken, profile, done) => {
+//     try {
+//       const email = profile.emails && profile.emails[0] ? profile.emails[0].value : null;
   
-      if (!email) {
-        // If no email is provided, return an error
-        return done(null, false, { message: 'Email is required to sign up' });
-      }
+//       if (!email) {
+//         // If no email is provided, return an error
+//         return done(null, false, { message: 'Email is required to sign up' });
+//       }
   
-      let user = await Userdb.findOne({ facebookId: profile.id });
-      if (!user) {
-        // New user signing up
-        user = new Userdb({
-          facebookId: profile.id,
-          username: profile.displayName,
-          email: email,
-          profilePicture: profile.photos && profile.photos[0] ? profile.photos[0].value : null // Check if photos exist
-        });
-        await user.save();
-        return done(null, { user, isNewUser: true });
-      }
-      // Existing user logging in
-      return done(null, { user, isNewUser: false });
-    } catch (err) {
-      done(err, null);
-    }
-  }));
+//       let user = await Userdb.findOne({ facebookId: profile.id });
+//       if (!user) {
+//         // New user signing up
+//         user = new Userdb({
+//           facebookId: profile.id,
+//           username: profile.displayName,
+//           email: email,
+//           profilePicture: profile.photos && profile.photos[0] ? profile.photos[0].value : null // Check if photos exist
+//         });
+//         await user.save();
+//         return done(null, { user, isNewUser: true });
+//       }
+//       // Existing user logging in
+//       return done(null, { user, isNewUser: false });
+//     } catch (err) {
+//       done(err, null);
+//     }
+//   }));
   
   
 
