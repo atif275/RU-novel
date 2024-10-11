a3ff3279f24965a808c3bb335a30931bda885743
zia101202 <136556867+zia101202@users.noreply.github.com>
2024-10-11 09:53:57 +0500

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -20,9 +20,9 @@ const PageHeader = () => {
   const theme = useSelector((state) => state.userData.theme);
   const barsClick = useSelector((state) => state.userData.barsClick);
   const user = useSelector((state) => state.userData.user);
-  const isAuthenticated = useSelector((state) => state.userData.isAuthenticated);
-  const navigate = useNavigate();
   
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector((state) => state.userData.isAuthenticated);
   const [dropdownVisible, setDropdownVisible] = useState(false);
 
   const toggleNavbar = () => {
@@ -83,7 +83,7 @@ const PageHeader = () => {
 
       if (response.ok) {
         const data = await response.json();
-        //console.log('my_data',data)
+        console.log('my_data',data)
         if (data.length === 0) {
           setNoMessages(true);
           setMessages([]);
@@ -94,8 +94,8 @@ const PageHeader = () => {
 
           const unreadMessages = data.length;
           const previousUnreadCount = parseInt(localStorage.getItem('lastKnownUnreadCount'), 10) ;
-           //console.log("hey",previousUnreadCount)
-           //console.log("bey",unreadMessages)
+           console.log("hey",previousUnreadCount)
+           console.log("bey",unreadMessages)
 
 
            if (!localStorage.getItem('lastKnownUnreadCount') || window.location.pathname === '/private/1') {
@@ -190,7 +190,7 @@ const PageHeader = () => {
                 // src="https://www.royaload.com/dist/img/logo/rr-logo-gold-white-small-min.png"
                 src="https://firebasestorage.googleapis.com/v0/b/ru-novel-images.appspot.com/o/border-images%2Flogo3.png?alt=media&token=5992bcd8-4adb-4c8c-89db-9e40ac5b2cef"
                 alt="RU Novel"
-                className="h-64 lg:h-64"
+                className="sm:h-64 sm:lg:h-64  h-[170px] w-[170px]"
               />
               
                 
@@ -199,7 +199,7 @@ const PageHeader = () => {
           </div>
   
           {/* Large Screen */}
-          <div className="hidden lg:flex items-center space-x-2">
+          <div className="flex items-center space-x-2">
             <ul className="flex space-x-4">
               <li className="relative">
                 <Link
@@ -341,142 +341,7 @@ const PageHeader = () => {
           </div>
   
           {/* Small Screen */}
-          <div className="flex items-center lg:hidden">
-            <Link
-              to="#"
-              className="flex text-gray-400 hover:text-[#23527C]"
-              aria-label="Notifications - 0 new"
-            >
-              {/* <FontAwesomeIcon icon={faBell} className="text-[18px] lg:text-[20px]" /> */}
-            </Link>
-            {isAuthenticated ? (
-               <>
-               <li className="relative block mt-5" ref={messagedownRef} >
-                  {/* <button
-                    onClick={toggleMessages}
-                    className="flex items-center text-gray-400 hover:text-[#23527C]"
-                    aria-label="Messages"
-                  >
-                    <FontAwesomeIcon icon={faEnvelope} className="text-[18px] lg:text-[20px]" />
-                  </button> */}
-                  {/* Messages div */}
-                  {/* {messagesVisible && (
-                    <div
-                      className={`absolute mt-2 p-4 text-[#bcc2cb] space-y-2 w-48 ${theme === 'dark' ? 'bg-[#181818]' : 'bg-gray-600'}`}
-                      style={{ zIndex: 10 }}
-                    >
-                      {noMessages ? (
-                        <p>No new messages.</p>
-                      ) : (
-                        messages
-                          .sort((a, b) => new Date(b.timestamp) - new Date(a.timestamp)) // Sorting messages in descending order by timestamp
-                          .map((message, index) => (
-                            <div key={index} className="p-2 border-b border-gray-500">
-                              <p className="font-bold">{message.sender}</p>
-                              <p>{message.subject}</p>
-                            </div>
-                          ))
-                      )}
-                    </div>
-                  )} */}
-                </li>
-              <div className="relative">
-                <button
-                  className={`flex items-center px-2 py-2 lg:py-3 text-sm ${theme === 'dark' ? 'text-white' : 'text-black'}`}
-                  onClick={toggleDropdown}
-                >
-                  <img
-                    src={profilePictureUrl}
-                    alt={user.username}
-                    className="w-7 h-7 lg:w-8 lg:h-8 rounded-full mr-2"
-                  />
-                  <span className="text-gray-500 font-bold hover:text-gray-400 mr-2">{user.username}</span>
-                  <FontAwesomeIcon icon={faChevronDown} className="mr-4 text-gray-500" />
-                </button>
-                {dropdownVisible && (
-                  <ul
-                    style={{ zIndex: 10 }}
-                    className={`absolute right-0 mt-2 text-[#bcc2cb] space-y-2 w-40 lg:w-48 text-sm ${theme === 'dark' ? 'bg-[#181818]' : 'bg-gray-600'}`}
-                  >
-                    <li>
-                      <Link to="/profile/525857" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faUser} className="mr-2 text-[#6fa7d7]" />
-                        My Profile
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/author-dashboard" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faPencil} className="mr-2 text-[#6fa7d7]" />
-                        Author Dashboard
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/private" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faEnvelopeOpen} className="mr-2 text-[#6fa7d7]" />
-                        Inbox
-                      </Link>
-                    </li>
-                    <li className="divider"></li>
-                    <li>
-                      <Link to="/my/follows" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faBook} className="mr-2 text-[#6fa7d7]" />
-                        Follow List
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/my/readlater" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faClock} className="mr-2 text-[#6fa7d7]" />
-                        Read Later
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/my/favorites" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faStar} className="mr-2 text-[#6fa7d7]" />
-                        Favorites
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to="/my/history" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faHistory} className="mr-2 text-[#6fa7d7]" />
-                        History
-                      </Link>
-                    </li>
-                    <li className="divider"></li>
-                    <li>
-                      <Link to="/account/options" className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faCogs} className="mr-2 text-[#6fa7d7]" />
-                        Settings
-                      </Link>
-                    </li>
-                    <li>
-                      <li  onClick={handleLogout} className="flex items-center px-3 py-1 hover:bg-[#55616f]">
-                        <FontAwesomeIcon icon={faSignOut} className="mr-2 text-[#6fa7d7]" />
-                        Log Out
-                      </li>
-                    </li>
-                  </ul>
-                )}
-              </div>
-              </>
-            ) : (
-              <div>
-                <Link
-                  to="/login"
-                  className="flex items-center mr-4 lg:mr-6 text-[#337AB7] p-4 lg:p-[20px] text-[12px] lg:text-[14px] hover:bg-[#EEEEEE] hover:text-[#23527C]"
-                >
-                  <FontAwesomeIcon icon={faSignIn} />
-                  <span className="ml-2">Log In</span>
-                </Link>
-              </div>
-            )}
-            <button
-              onClick={toggleNavbar}
-              className="flex items-center text-[14px] text-[#337AB7]  p-1 rounded-lg focus:outline-none hover:border-[#337AB7] lg:hidden"
-              aria-label="Toggle navigation"
-            >
-              <FontAwesomeIcon icon={faBars} className='ml-2' />
-            </button>
-          </div>
+       
         </div>
       </header>
     );
